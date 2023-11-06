e509135f3629fa5caa3abf1dc17dce5b79aecb0b
Mahmoud Hassan <abosamour990@gmail.com>
2023-11-06 18:13:37 +0200

    merg

--- smart_School/smart_School/settings.py
+++ smart_School/smart_School/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'smart_School.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+  
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
